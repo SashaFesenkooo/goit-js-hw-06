b00b6a4c54d2351bc8724f5b00858e6c18d8271d
Sasha Fesenko <sashafesenkooo@gmail.com>
2022-12-25 22:10:24 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,29 @@ const ingredients = [
   'Condiments',
 ];
 
+
 const ingredientsList = document.querySelector("#ingredients");
+
 ingredientsList.append(
-  ...ingredients.map(item =>{
-    const itemList = document.createElement("li");
+  ...ingredients.map((item) => {
+    let itemList = document.createElement("li");
     itemList.textContent = item;
     return itemList;
   })
-);
+); 
+
+
+// const ingredientsList = document.querySelector('#ingredients');
+
+// const ingredient = ingredients.map(elem => {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = elem;
+//   return liEl;
+// });
+
+// ingredientsList.append(...ingredient);
+
+
+
+
+
