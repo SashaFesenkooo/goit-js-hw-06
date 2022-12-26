616ead9cdd3d85bfa23665195fc7ca3a0a45aa2d
Sasha Fesenko <sashafesenkooo@gmail.com>
2022-12-26 12:28:00 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+const formEl = document.querySelector(".login-form");
+console.log(formEl); 
+  
+ formEl.addEventListener("submit", handleSubmit); 
+  
+ function handleSubmit(event) { 
+   event.preventDefault(); 
+   const { 
+     elements: { email, password }, 
+   } = event.currentTarget; 
+   if (email.value === "" || password.value === "") { 
+     return alert("Please fill in all the fields!"); 
+   } 
+   const formElEntereddValues = {}; 
+   (formElEntereddValues.email = email.value), 
+     (formElEntereddValues.password = password.value); 
+   console.log(formElEntereddValues); 
+   event.currentTarget.reset(); 
+ }
